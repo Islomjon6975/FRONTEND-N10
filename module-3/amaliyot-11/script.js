3bfc3e501c3315e152ff2d8ace8ae0f4412a9f87
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2024-05-08 07:51:24 +0500

    module-3: amaliyot-10

---
+++ module-3/amaliyot-11/script.js
@@ -0,0 +1,69 @@
+// let skills = ["html", "css", "js", "react", "typescript", "next js"];
+
+// skills.forEach((value, index, array) => {
+//    console.log(value);
+// });
+
+// for (let item of skills) {
+//    console.log(item);
+// }
+
+// for (let i = 0; i < skills.length; i++) {
+//    console.log(skills[i]);
+// }
+
+// function User(first_name, last_name, age) {
+//    this.first_name = first_name;
+//    this.last_name = last_name;
+//    this.age = age;
+//    this.user_data = function () {
+//       console.log("Ism: ", this.first_name);
+//       console.log("Familiya: ", this.last_name);
+//       console.log("Yosh: ", this.age);
+//    };
+// }
+
+// const user1 = new User("Alisher", "Olimov", 30);
+// user1.user_data();
+
+// function Numbers(list) {
+//    // {list: list, count: 0, sum: function() {}}
+//    this.list = list;
+//    this.count = 0;
+
+//    this.sum = function () {
+//       //  for (let item of this.list) {
+//       //     this.count += item;
+//       //  }
+
+//       this.count = this.list.reduce((sum, value) => sum + value, 0);
+//    };
+// }
+
+// const numbers1 = new Numbers([1, 2, 3, 4]);
+// numbers1.sum();
+// console.log(numbers1.count);
+
+[
+   {
+      _id: 1,
+      name: "Pen",
+      quantity: 2,
+      price: 4000,
+      color: "black",
+   },
+   {
+      _id: 2,
+      name: "Pencil",
+      quantity: 3,
+      price: 2000,
+      color: "blue",
+   },
+   {
+      _id: 3,
+      name: "Eraser",
+      quantity: 1,
+      price: 1000,
+      color: "red",
+   },
+];
